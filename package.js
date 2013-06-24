2b1a0ee9d7441317aa1dc9cf473d7029b63fe970
Andrew Mao <mizzao@gmail.com>
2013-06-24 16:45:57 -0400

    refactored to use an npm pull-in instead of a static file

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
     summary: "Programmatic dialog boxes using Twitter's bootstrap modals"
 });
 
+Npm.depends({bootbox: "3.2.0"});
+
 Package.on_use(function (api) {
     api.use('jquery', 'client');
-    api.add_files([
-        'lib/bootbox.js'
-    ], 'client');
+    api.add_files('.npm/node_modules/bootbox/bootbox.min.js', 'client');
 });
