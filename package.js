5ac8dbc736b6058f9a09848a8f1749513fd7613d
Andrew Mao <mizzao@gmail.com>
2013-06-25 10:21:46 -0400

    add explicit bootstrap dependency

--- package.js
+++ package.js
@@ -5,6 +5,8 @@ Package.describe({
 Npm.depends({bootbox: "3.2.0"});
 
 Package.on_use(function (api) {
+    api.use('bootstrap', 'client');
     api.use('jquery', 'client');
+
     api.add_files('.npm/node_modules/bootbox/bootbox.min.js', 'client');
 });
