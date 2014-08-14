62a2d3de9efe3a3b3880a8dc8e00c29b74251ffe
Andrew Mao <mizzao@gmail.com>
2014-08-14 14:07:15 -0400

    4.3.0 release for Meteor 0.9

--- package.js
+++ package.js
@@ -1,8 +1,12 @@
 Package.describe({
-    summary: "Programmatic dialog boxes using Twitter's bootstrap modals"
+  summary: "Programmatic dialog boxes using Twitter's bootstrap modals",
+  version: "4.3.0",
+  git: "https://github.com/TimHeckel/meteor-bootboxjs.git"
 });
 
-Package.on_use(function (api) {
-    api.use('jquery', 'client');
-    api.add_files('lib/bootbox.js', "client");
+Package.onUse(function (api) {
+  api.versionsFrom("METEOR-CORE@0.9.0-atm");
+
+  api.use('jquery', 'client');
+  api.add_files('lib/bootbox.js', "client");
 });
